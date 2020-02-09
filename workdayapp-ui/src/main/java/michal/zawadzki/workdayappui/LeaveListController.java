cd023790cd423902c5c20287d09c2d251e2b6f27
mzawadzki90 <michauzawadzki@gmail.com>
2020-02-09 23:50:18 +0100

    Fill table view with data

--- workdayapp-ui/src/main/java/michal/zawadzki/workdayappui/LeaveListController.java
+++ workdayapp-ui/src/main/java/michal/zawadzki/workdayappui/LeaveListController.java
@@ -4,17 +4,61 @@
 */
 package michal.zawadzki.workdayappui;
 
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableCell;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.util.Callback;
 import michal.zawadzki.workdayappclient.WorkdayappClient;
+import michal.zawadzki.workdayappclient.api.leave.LeaveRequestDto;
 import org.springframework.stereotype.Component;
+
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
 
 @Component
 public class LeaveListController {
 
+    private final static List<String> DATE_COLUMN_IDS =
+            Arrays.asList("leaveRequestSince", "leaveRequestTill", "leaveRequestCreatedAt",
+                          "leaveRequestModifiedAt");
+
     private final WorkdayappClient workdayappClient;
+
+    @FXML
+    public TableView<LeaveRequestDto> leaveRequests;
 
     public LeaveListController(WorkdayappClient workdayappClient) {
         this.workdayappClient = workdayappClient;
     }
 
+    @FXML
+    public void initialize() {
+        leaveRequests.getColumns().stream().filter(column -> DATE_COLUMN_IDS.contains(column.getId()))
+                     .map(column -> (TableColumn<LeaveRequestDto, Date>) column)
+                     .forEach(dateColumn -> dateColumn.setCellFactory(dateCellFactory()));
+
+        final ObservableList<LeaveRequestDto> items = leaveRequests.getItems();
+        items.addAll(workdayappClient.listByWorkerId(1).getLeaveRequests());
+    }
+
+    private Callback<TableColumn<LeaveRequestDto, Date>, TableCell<LeaveRequestDto, Date>> dateCellFactory() {
+        return column -> new TableCell<>() {
+            private final SimpleDateFormat format = new SimpleDateFormat("dd.MM.yyyy");
+
+            @Override
+            protected void updateItem(Date item, boolean empty) {
+                super.updateItem(item, empty);
+                if (empty) {
+                    setText(null);
+                } else {
+                    setText(format.format(item));
+                }
+            }
+        };
+    }
 
 }
